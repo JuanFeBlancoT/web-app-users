1cc65d6107c1c7142be15fb77b8a6cf967e3433c
DaniloErazo <erazodanilo1218@gmail.com>
2022-02-06 18:38:27 -0500

    Created User class

---
+++ web-app-users/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_app_users.Models
+{
+    public class Class
+    {
+        public String ID { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BornDate { get; set; }
+        public String Password { get; set; }
+    }
+}
